e206fad80f43218c61b2745c9f9c9010a8e5a749
Fnu Butul Parveen <89315756+Butulparveen@users.noreply.github.com>
2024-10-20 20:04:45 -0700

    Update ifelse.py

--- ifelse.py
+++ ifelse.py
@@ -1,6 +1,13 @@
-name = "Python"
-if name == "Python":
-    print ("Hello,", name)
-else:
-    print("Oh well what is your name?")
+# name = "Python"
+# if name == "Python":
+#     print ("Hello,", name)
+# else:
+#     print("Oh well what is your name?")
   
+
+name = "python"
+if name == "python":
+        print("Hello,", name)
+else:
+    name1 = input("Enter your name ":)
+    print("oh well my name is",name1)
