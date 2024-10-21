b6f78a8b569c9195d9b37cc1d24770c6cd1dd8d0
Fnu Butul Parveen <89315756+Butulparveen@users.noreply.github.com>
2024-10-20 19:53:10 -0700

    Update sum.py

--- sum.py
+++ sum.py
@@ -1,4 +1,10 @@
-num1 = 4
-num2 = 5
-sum = num1 + num2 
-print (" Sum of ", num1, num2, "is", sum)
+# num1 = 4
+# num2 = 5
+# sum = num1 + num2 
+# print (" Sum of ", num1, num2, "is", sum)
+
+
+a = input("first number: ")
+b = input("\nSecond number:")
+sum = float(a) + float(b)
+print ("The sum of {0} & {1} is {2}" .format(a, b, sum))
