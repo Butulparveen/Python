54c89e0545f3972037247c2d1c6233d323cf2fa3
Butul parveen <butulparveen@Butuls-MacBook-Pro.local>
2024-10-22 20:45:40 -0700

    program to find smallest and largest number in a list

---
+++ smalllargest.py
@@ -0,0 +1,6 @@
+list_numbers = [4,1,5,3,9]
+sorted_list = sorted(list_numbers)
+print("List ",list_numbers)
+print("After sorting the list",sorted_list)
+print("The smallest number from list",sorted_list[0])
+print("The largest number form list",sorted_list[-1])
