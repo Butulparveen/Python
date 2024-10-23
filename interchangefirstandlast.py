3244409059eb387d96882e29227737b934de32df
Butul parveen <butulparveen@Butuls-MacBook-Pro.local>
2024-10-22 18:54:58 -0700

    interchange first and last elements in a list

---
+++ interchangefirstandlast.py
@@ -0,0 +1,4 @@
+interchange_list = [3,1,5,6,2]
+interchange_list[0],interchange_list[-1] = interchange_list[-1],interchange_list[0]
+print("After Interchange first and last elements in a list:",interchange_list)
+
