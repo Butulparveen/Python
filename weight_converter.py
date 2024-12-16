5c71df190e7c391749a67cb245620daf79407019
Butul parveen <butulparveen@Butuls-MacBook-Pro.local>
2024-12-16 13:34:32 -0800

    Weight calculator

--- weight_converter.py
+++ weight_converter.py
@@ -4,9 +4,10 @@ unit = input("Kilograms or pounds,(K or L) :")
 if unit == "K":
     weight = weight * 2.205
     unit = "Lbs"
+    print(f"Your weight is:{round(weight,2)} {unit}")
 elif unit == "L":
     weight = weight / 2.205
     unit = "Kgs"
+    print(f"Your weight is:{round(weight,2)} {unit}")
 else:
     print(f"{unit} is not valid")
-print(f"Your weight is:{round(weight,2)} {unit}")
