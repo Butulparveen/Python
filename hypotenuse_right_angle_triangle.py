87061f909541a1d13d07929cbdcc1162d7a81eb1
Butul parveen <butulparveen@Butuls-MacBook-Pro.local>
2024-12-16 12:39:47 -0800

    hypotenuse_right_angle_triangle sqrt. c =sqrta^2+b^2

---
+++ hypotenuse_right_angle_triangle.py
@@ -0,0 +1,8 @@
+import math
+
+a = float(input('Enter the sida A:'))
+b = float(input('Enter the side B:'))
+
+c = math.sqrt(pow(a,2)+pow(b,2))
+
+print(f"Side C value of hypotenuse_right_angle_triangle :{c}")
