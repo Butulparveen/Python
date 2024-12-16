cfad71fc6dd765dd1093eb68ab941297adfc798c
Butul parveen <butulparveen@Butuls-MacBook-Pro.local>
2024-12-16 12:55:56 -0800

    User like food

---
+++ user_like_some_food.py
@@ -0,0 +1,7 @@
+#== is the comparision operator where as = assignment operator.
+
+response = input('Would you like food? Y/N:')
+if response == 'Y':
+    print("Have some food")
+else:
+    print("No food for you")
